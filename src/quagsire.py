1dc085de9f90579dbc335ddfcee231bad9b5dfe1
Jake Lee <jlee211@ucsc.edu>
2024-09-09 15:39:51 -0700

    init

---
+++ src/quagsire.py
@@ -0,0 +1,38 @@
+"""quagsire.py
+
+Feed me thermal image data of plants and I'll feed you info about water content.
+
+@date 09/09/2024
+"""
+import argparse
+import cv2
+import numpy as np
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i",
+        "--input",
+        default="flight.tif",
+        help="Provide an input file for eating.",
+        type=str
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        default="report.md",
+        help="Regurgitated output file.",
+        type=str
+    )
+    return parser.parse_args()
+
+def quagsire(args: argparse.Namespace):
+    """
+    Ingest thermal imagery and spit out info on water content.
+    """
+    print([arg for arg in args])
+
+if __name__ == "__main__":
+    args = parse_args()
+    quagsire(args)
