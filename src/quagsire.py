e127ded1b520f718be066c3a2a07d49e6d56b9bf
Jake Lee <jlee211@ucsc.edu>
2024-09-10 11:55:49 -0700

    skeleton for temp analysis

--- src/quagsire.py
+++ src/quagsire.py
@@ -9,6 +9,10 @@ import cv2
 import numpy as np
 
 
+# Typedefs.
+type Point = tuple[int, int]
+
+
 # Setup.
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
@@ -28,7 +32,8 @@ def parse_args() -> argparse.Namespace:
     )
     return parser.parse_args()
 
-# Data ingestion.
+# Data processing.
+## Image ingestion.
 def gray16_to_gray8(gray16: np.array) -> np.array:
     """Convert an image into a Numpy array representing"""
     gray8 = np.zeros((160, 120), dtype=np.uint8)
@@ -38,15 +43,36 @@ def gray16_to_gray8(gray16: np.array) -> np.array:
 def img_read(ipath: str) -> [np.array, np.array]:
     """Convert an image file into some iterable object.
     """
-    # Convert image into a gray8 format.
+    # Convert image into:
+    #   1. gray8 format for display.
+    #   2. gray16 format for thermal analysis.
     try:
         gray16 = cv2.imread(ipath, cv2.IMREAD_ANYDEPTH)
         gray8 = gray16_to_gray8(gray16)
     except TypeError as e:
+        print(e)
         gray16 = np.zeros((160, 120), dtype=np.uint16) 
         gray8 = np.zeros((160, 120), dtype=np.uint8) 
-        print(e)
     return gray8, gray16
+
+## Image analysis.
+def img_to_tmtx(therm: np.array) -> np.array, Point, Point:
+    """Convert a gray16 image into a matrix of temperatures represented by each
+        pixel.
+
+        Returns:
+            * t_matrix  (np.array)  : Assign each pixel a temperature [C].
+            * t_min     (Point)     : Index of pixel with min temperature.
+            * t_max     (Point)     : Index of pixel with max temperature.
+    """
+    pass
+
+def img_analyze(img: np.array, therm: np.array):
+    """Find the following about a given image:
+        1. Max/Min indices and their temperatures.
+        2. CWSI matrix for entire plot.
+    """
+    t_matrix, t_min, t_max = img_to_tmtx(therm)
 
 
 # Display.
@@ -59,8 +85,8 @@ def display(
         cv2.waitKey(0)
     except cv2.error as e:
         print(e)
-        print("Hi nub.")
 
+# Clean up.
 def clean_up():
     cv2.destroyAllWindows()
 
@@ -70,6 +96,7 @@ def quagsire(args: argparse.Namespace):
     Ingest thermal imagery and spit out info on water content.
     """
     img, therm = img_read(args.input)
+    img_analyze(img, therm)
     display(img)
     clean_up()
 
