2ac739927afac2a50cd734ec67d36432826c1ec9
Jake Lee <jlee211@ucsc.edu>
2024-09-10 11:38:16 -0700

    added dummy for testing bad data

--- src/quagsire.py
+++ src/quagsire.py
@@ -29,17 +29,49 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 # Data ingestion.
-def img_read(ipath: str) -> list(str):
-    pass
+def gray16_to_gray8(gray16: np.array) -> np.array:
+    """Convert an image into a Numpy array representing"""
+    gray8 = np.zeros((160, 120), dtype=np.uint8)
+    gray8 = cv2.normalize(gray16, gray8, 0, 255, cv2.NORM_MINMAX)
+    gray8 = np.uint8(gray8)
 
+def img_read(ipath: str) -> [np.array, np.array]:
+    """Convert an image file into some iterable object.
+    """
+    # Convert image into a gray8 format.
+    try:
+        gray16 = cv2.imread(ipath, cv2.IMREAD_ANYDEPTH)
+        gray8 = gray16_to_gray8(gray16)
+    except TypeError as e:
+        gray16 = np.zeros((160, 120), dtype=np.uint16) 
+        gray8 = np.zeros((160, 120), dtype=np.uint8) 
+        print(e)
+    return gray8, gray16
+
+
+# Display.
+def display(
+        img: np.array,
+        title: str = "Test"
+    ):
+    try:
+        cv2.imshow(title, img)
+        cv2.waitKey(0)
+    except cv2.error as e:
+        print(e)
+        print("Hi nub.")
+
+def clean_up():
+    cv2.destroyAllWindows()
 
 # Main.
 def quagsire(args: argparse.Namespace):
     """
     Ingest thermal imagery and spit out info on water content.
     """
-    img_data = img_read(arg.input)
-    print(img_data)
+    img, therm = img_read(args.input)
+    display(img)
+    clean_up()
 
 if __name__ == "__main__":
     args = parse_args()
